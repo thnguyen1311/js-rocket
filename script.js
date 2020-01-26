eb6710e8e884f21ea6d34cb99cd4928a33b2c450
thnguyen1311 <thnguyen1311@users.noreply.github.com>
2020-01-27 00:41:33 +0700

    Made my rocket

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
- var car ={
+//This function changes class name
+var doCoolStuff = function(){
+    var currentClassName = document.getElementById('cool').className;
+    if(currentClassName == 'cool'){
+        document.getElementById('cool').className ='cool red';
+    }else{
+        document.getElementById('cool').className='cool';
+    }
+} 
+
+
+var car ={
             make: 'VW',
             type: 'Polo',
             color: 'blue',
